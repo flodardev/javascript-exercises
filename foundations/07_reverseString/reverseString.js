3737930d714e6b9b1f29d8d44df6529ec5bcc5b4
flodardev <63950579+flodardev@users.noreply.github.com>
2026-01-17 12:02:16 +0800

    foundations/07_reverseString complete

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reverseWord = "";
+    let lengthWord = word.length - 1;
+    let wordArray = word.split("");
 
+    for (i = lengthWord; i >= 0; i--) {
+        reverseWord += wordArray[i];
+    }
+
+    return reverseWord;
 };
 
 // Do not edit below this line
