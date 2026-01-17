33590a3e66fee653c5d66f01ad7e4d5bf4787356
flodardev <63950579+flodardev@users.noreply.github.com>
2026-01-17 11:31:41 +0800

    foundations/06_repeatString complete

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, numbers) {
+    if (numbers < 0) {
+        return "ERROR"
+    }
 
+    let strings = "";
+
+    for (let i = 0; i < numbers; i++) {
+        strings += string;
+    }
+
+    return strings;
 };
 
 // Do not edit below this line
