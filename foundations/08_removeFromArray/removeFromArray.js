0867ed66b4f95b73c8cd6ee915559d86de40b3fa
flodardev <63950579+flodardev@users.noreply.github.com>
2026-01-17 16:10:35 +0800

    foundations/08_removeFromArray complete

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
 const removeFromArray = function() {
-};
+    let argArray = Array.from(arguments);
+    let array = argArray[0];
+    let remove = argArray.splice(1);
+    let result = array.filter(x => !remove.includes(x))
+    return result;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
