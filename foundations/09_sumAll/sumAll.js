9739e5743cde3f682ce817fc8a593856e87a2635
flodardev <63950579+flodardev@users.noreply.github.com>
2026-01-22 14:20:43 +0800

    foundations/09_sumAll complete

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(...args) {
 
+    for (arg of args) {
+        if (!Number.isInteger(arg)
+            || arg < 0
+        ) {
+            return "ERROR"
+        }
+    }s
+
+    let arraySorted = [...args].sort((a,b) => a - b)
+    let newArray = [];
+    for (let i = arraySorted[0]; i <= arraySorted[1]; i++) {
+        newArray.push(i);
+    }
+    return newArray.reduce((sum, current) => sum + current);
 };
 
 // Do not edit below this line
