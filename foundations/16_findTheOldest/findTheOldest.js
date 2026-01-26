0522317bf91beab8567af3a100975837f6d49e65
flodardev <63950579+flodardev@users.noreply.github.com>
2026-01-26 16:59:16 +0800

    exercise foundations/16_findTheOldest complete

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const checkYear = arr.map(item => {
+        if (!item["yearOfDeath"]) {
+            item["yearOfDeath"] = new Date().getFullYear();
+        }
+        return item;
+    })
+
+    console.log(checkYear);
+    const oldest = checkYear.sort((personOne, personTwo) => {
+        let personOneAge = personOne.yearOfDeath - personOne.yearOfBirth;
+        let personTwoAge = personTwo.yearOfDeath - personTwo.yearOfBirth;
+        return personTwoAge - personOneAge;
+    })
+
+    // console.log(oldest);
+    return oldest[0];
 
 };
 
