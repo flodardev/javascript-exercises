bdc17c390edff782890384004340619cc43bfe79
flodardev <63950579+flodardev@users.noreply.github.com>
2026-01-26 16:33:34 +0800

    exercise foundations/14_fibonacci complete

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let indexInt = Number(index);
+    let isIndex = false;
+    let indexNumber = 1;
+    let currentFibonacciNumber = 1;
+    let previousFibonacciNumber = 0;
+
+    while (!isIndex) {
+        if (indexInt === 0) {
+            return previousFibonacciNumber;
+        } else if (indexInt < 0) {
+            return "OOPS";
+        } else {
+            if (indexNumber === indexInt) {
+                return currentFibonacciNumber;
+            }
+        }
+
+        let temp = currentFibonacciNumber;
+        currentFibonacciNumber += previousFibonacciNumber;
+        previousFibonacciNumber = temp;
+        ++indexNumber;
+    }
 
 };
 
