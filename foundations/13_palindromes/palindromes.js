bb3fe69f29b5e19df853f4e887148a55ec250322
flodardev <63950579+flodardev@users.noreply.github.com>
2026-01-26 15:53:21 +0800

    exercise foundations/13_palindromes complete

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const onlyChar = string.replace(/[^a-zA-Z0-9]/g, '');
 
+    const lowerCased = onlyChar.toLowerCase();
+    const backwards = lowerCased.split("").reverse().join("");
+    if (lowerCased.localeCompare(backwards) === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
